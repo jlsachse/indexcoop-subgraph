e1c1f4fc7eb712d81edac0fb6016de43dcabda71
Timur Badretdinov <destinerx@gmail.com>
2019-09-22 18:17:00 +0300

    Save full set data

--- src/proxy_v1.ts
+++ src/proxy_v1.ts
@@ -1,14 +1,23 @@
 import { Bytes, BigInt } from "@graphprotocol/graph-ts"
 
 import { LogRebalancingSetIssue } from "../generated/ProxyV1/TokenSetProxyV1"
+import { Set as SetContract } from "../generated/ProxyV1/templates/Set/Set"
 import { Set } from "../generated/schema"
 
 export function handleSetIssue(event: LogRebalancingSetIssue): void {
 	let address = event.params.rebalancingSetAddress;
 	let set = Set.load(address.toHexString());
 	if (!set) {
+		let setContract = SetContract.bind(address);
 		set = new Set(address.toHexString());
+
 		set.address = address;
+		set.name = setContract.name();
+		set.symbol = setContract.symbol();
+		set.supply = setContract.totalSupply();
+		set.components = setContract.getComponents() as Array<Bytes>;
+		set.units = setContract.getUnits();
+		set.naturalUnit = setContract.naturalUnit();
 	}
 	set.save();
 }
