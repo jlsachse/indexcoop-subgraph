388feb6c571e7cf521782d5a58b5fa0e43327272
Timur Badretdinov <destinerx@gmail.com>
2019-09-23 16:26:57 +0300

    RebalanceStarted handler

--- src/set.ts
+++ src/set.ts
@@ -1,8 +1,7 @@
 import { Address } from "@graphprotocol/graph-ts"
 
 import { Set, Issuance, Redemption, Rebalance } from "../generated/schema"
-import { Transfer, Set as SetContract }  from "../generated/SetCore/templates/Set/Set"
-import { SetCore as SetCoreContract } from "../generated/SetCore/SetCore"
+import { Transfer, RebalanceStarted, Set as SetContract }  from "../generated/SetCore/templates/Set/Set"
 
 export function handleTransfer(event: Transfer): void {
 	let id = event.transaction.hash.toHexString() + '-' + event.logIndex.toString();
@@ -28,18 +27,6 @@ export function handleTransfer(event: Transfer): void {
 			set.supply = setContract.totalSupply();
 			set.save();
 		}
-
-		if (isSet(to)) {
-			// rebalance settled
-			let set = Set.load(setAddress.toHexString());
-			let rebalances = set.rebalances as Array<String>;
-			let rebalanceId = rebalances[rebalances.length - 1];
-			let rebalance = Rebalance.load(rebalanceId);
-			rebalance.toSet = to.toHexString();
-			rebalance.toAmount = value;
-			rebalance.settleTimestamp = event.block.timestamp;
-			rebalance.save();
-		}
 	}
 	if (to.toHexString() == zeroAddress) {
 		if (isTokenSet(setAddress)) {
@@ -56,29 +43,21 @@ export function handleTransfer(event: Transfer): void {
 			set.supply = setContract.totalSupply();
 			set.save();
 		}
-
-		if (isSet(from)) {
-			// rebalance started
-			let rebalance = new Rebalance(id);
-			rebalance.set_ = setAddress.toHexString();
-			rebalance.fromSet = from.toHexString();
-			rebalance.fromAmount = value;
-			rebalance.startTimestamp = event.block.timestamp;
-			rebalance.save();
-		}
 	}
 }
 
-function isSet(address: Address): boolean {
-	let setCoreAddress = Address.fromString('0xf55186CC537E7067EA616F2aaE007b4427a120C8');
-	let setCoreContract = SetCoreContract.bind(setCoreAddress);
-	let setTokens = setCoreContract.setTokens();
-	for (let i = 0; i < setTokens.length; i++) {
-		if (setTokens[i].toHexString() == address.toHexString()) {
-			return true;
-		}
+export function handleRebalance(event: RebalanceStarted): void {
+	let id = event.transaction.hash.toHexString() + '-' + event.logIndex.toString();
+	let setAddress = event.address;
+
+	if (isTokenSet(setAddress)) {
+		let rebalance = new Rebalance(id);
+		rebalance.set_ = setAddress.toHexString();
+		rebalance.oldSet = event.params.oldSet;
+		rebalance.newSet = event.params.newSet;
+		rebalance.timestamp = event.block.timestamp;
+		rebalance.save();
 	}
-	return false;
 }
 
 function isTokenSet(address: Address): boolean {
