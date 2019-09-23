1292c9921297ad692c5f79c8be8fb3f18fb0760f
Timur Badretdinov <destinerx@gmail.com>
2019-09-23 17:14:07 +0300

    Save non-TokenSets

--- src/core.ts
+++ src/core.ts
@@ -1,8 +1,24 @@
+import { Bytes } from "@graphprotocol/graph-ts"
+
 import { SetTokenCreated }  from "../generated/SetCore/SetCore"
+import { Set as SetContract } from "../generated/SetCore/templates/Set/Set"
 import { Set as SetDataSource } from "../generated/SetCore/templates"
+import { Set } from "../generated/schema"
 
 export function handleSetCreated(event: SetTokenCreated): void {
 	let address = event.params._setTokenAddress;
+
+	let set = new Set(address.toHexString());
+	let setContract = SetContract.bind(address);
+
+	set.address = address;
+	set.name = setContract.name();
+	set.symbol = setContract.symbol();
+	set.supply = setContract.totalSupply();
+	set.components = setContract.getComponents() as Array<Bytes>;
+	set.units = setContract.getUnits();
+	set.naturalUnit = setContract.naturalUnit();
+	set.save();
 
 	SetDataSource.create(address);
 }
--- src/proxy_v2.ts
+++ src/proxy_v2.ts
@@ -1,23 +1,11 @@
-import { Bytes, BigInt } from "@graphprotocol/graph-ts"
-
 import { LogPayableExchangeIssue } from "../generated/ProxyV2/TokenSetProxyV2"
-import { Set as SetContract } from "../generated/SetCore/templates/Set/Set"
-import { Set } from "../generated/schema"
+import { TokenSet } from "../generated/schema"
 
 export function handleSetIssue(event: LogPayableExchangeIssue): void {
 	let address = event.params.rebalancingSetAddress;
-	let set = Set.load(address.toHexString());
-	if (!set) {
-		let setContract = SetContract.bind(address);
-		set = new Set(address.toHexString());
-
-		set.address = address;
-		set.name = setContract.name();
-		set.symbol = setContract.symbol();
-		set.supply = setContract.totalSupply();
-		set.components = setContract.getComponents() as Array<Bytes>;
-		set.units = setContract.getUnits();
-		set.naturalUnit = setContract.naturalUnit();
+	let tokenSet = TokenSet.load(address.toHexString());
+	if (!tokenSet) {
+		tokenSet.set_ = address.toHexString();
+		tokenSet.save();
 	}
-	set.save();
 }
