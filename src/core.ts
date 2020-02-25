2aa5467e39d00b954fcf80ca334d968cab956513
Timur Badretdinov <destinerx@gmail.com>
2020-02-25 16:24:14 +0300

    Add 4 social sets

--- src/core.ts
+++ src/core.ts
@@ -60,7 +60,9 @@ function _isTokenSet(address: Address): boolean {
 	let socialSets: Array<String> = [
 		'0x09ae0c4c34a09875660e681fe1890f3b35175151',
 		'0x28c6a58c2a5d8c5f6681e07bfa0ada4bea14c9ee',
+		'0x41f3b2b6d4d122e81834582a3f3367388def95cf',
 		'0x5958107be1ecc8d0d1593b3d7cc75f5e48304682',
+		'0x6ad29bde4ecc382e8d535206f1f0dd92a6c034df',
 		'0x73104e9d3da91e410a6c211068f7bffabbbd3e26',
 		'0x78e14b9a8d006ec7e23988a0a87263569c3f4839',
 		'0x7b8d9e95caa85d7905c3435da1ba9fa7f79bf4fa',
@@ -68,6 +70,7 @@ function _isTokenSet(address: Address): boolean {
 		'0x8c7c047051f37746bb1dcd16ae37851a3891635c',
 		'0x8d66df717faf06c07b5d3e53ffd3ea66d612e150',
 		'0x90f49083ff588ec5a5459f4d2a64b8d409c03122',
+		'0xaaa4eb35d08dbb9b1b8760cb9ffe77b9023d7511',
 		'0xabc754ac2161b557d28062f41dcc0fc18440ac7e',
 		'0xac6560df686f3ac7039b0dd6867c874c99d9da06',
 		'0xb272422b7d07b94859ad61e4435c6d5087f308de',
@@ -77,6 +80,7 @@ function _isTokenSet(address: Address): boolean {
 		'0xd9c6d58ec4b6045067190042999351889fd27eb7',
 		'0xdbf5c7d8ac5007667617a15db2c1b1d616c9d302',
 		'0xdc5bbcf8189a69337f693a9f3f63ae37789ede92',
+		'0xdd7ae9466c9caad4bf632f796eaf4287f9616a7a',
 		'0xe33a35fd5cad21ce72ebbc8a2885bdd0983de043',
 		'0xe86811516f9e46f6f2a8a19754c893ded414d682',
 	];
